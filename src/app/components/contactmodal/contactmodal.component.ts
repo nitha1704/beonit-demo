7bdd30cd0d8d7c6973e5e953cfca61987073f283
Nitha <alive1704@gmail.com>
2021-08-30 04:10:31 +0700

    fix contactmodal

--- src/app/components/contactmodal/contactmodal.component.ts
+++ src/app/components/contactmodal/contactmodal.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-contactmodal',
-  templateUrl: './contactmodal.component.html',
-  styleUrls: ['./contactmodal.component.scss']
+  selector: "app-contactmodal",
+  templateUrl: "./contactmodal.component.html",
+  styleUrls: ["./contactmodal.component.scss"],
 })
 export class ContactmodalComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   closeModal() {
     const wrapContactModal = document.querySelector(".wrap-contact-modal");
-    wrapContactModal.classList.remove('active')
+    wrapContactModal.classList.remove("active");
   }
 
+  addFocus(e) {
+    e.target.parentNode.classList.add("input-group-focus");
+  }
+  removeFocus(e) {
+    e.target.parentNode.classList.remove("input-group-focus");
+  }
 }

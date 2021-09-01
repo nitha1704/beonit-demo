6f9c1181abdb093be566a17e6a28546d87335d11
Nitha <alive1704@gmail.com>
2021-09-02 02:05:10 +0700

    add owl carousel, edit phone number company

--- src/app/examples/landingpage/landingpage.component.ts
+++ src/app/examples/landingpage/landingpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import { OwlOptions } from "ngx-owl-carousel-o";
 
 @Component({
   selector: "app-landingpage",
@@ -8,14 +9,39 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 export class LandingpageComponent implements OnInit, OnDestroy {
   constructor() {}
 
-  ngOnInit() {
-
-  }
-  ngOnDestroy() {
-
-  }
+  ngOnInit() {}
+  ngOnDestroy() {}
 
   openModal(wrapContactModal) {
-    wrapContactModal.classList.add('active');
+    wrapContactModal.classList.add("active");
   }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 0,
+    dotsSpeed: 0,
+    navText: [
+      '<a class="left carousel-control carousel-control-prev" role="button" tabindex="0"><span aria-hidden="true" class="icon-prev carousel-control-prev-icon"></span><!----><span class="sr-only">Previous</span></a>',
+      '<a class="right carousel-control carousel-control-next" role="button" tabindex="0"><span aria-hidden="true" class="icon-next carousel-control-next-icon"></span><span class="sr-only">Next</span></a>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      740: {
+        items: 1,
+      },
+      940: {
+        items: 1,
+      },
+    },
+    nav: true,
+  };
 }
